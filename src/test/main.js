fc10c2803486ce2e002ed16359336e25098ab9be
Pavel Gabriel <alovak@gmail.com>
2018-04-22 16:58:33 +0300

    move files

---
+++ src/test/main.js
@@ -0,0 +1,31 @@
+import Channel from './utils/channel';
+import iframe from './utils/iframe';
+
+var channel;
+
+function inIframe () {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
+}
+
+if (inIframe()) {
+  channel = new Channel({ label: 'iframe' });
+  channel.ping();
+  channel.connect({ target: window.parent });
+  channel.ready();
+} else {
+  channel = new Channel({ label: 'parent' });
+  channel.ping();
+  window.addEventListener('DOMContentLoaded', inParent);
+}
+
+function inParent() {
+  const container = document.getElementById('container');
+  const iframeEl = new iframe({ src: '/conn-iframe.html', rnd: true, height: '100px' });
+  container.appendChild(iframeEl);
+  channel.connect({ target: iframeEl.contentWindow });
+  channel.ready();
+}
