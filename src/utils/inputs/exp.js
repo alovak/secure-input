b766f6f302028117adf5e7266eabfdc461f7cc02
Pavel Gabriel <alovak@gmail.com>
2018-05-10 09:24:47 +0300

    validate exp date

--- src/utils/inputs/exp.js
+++ src/utils/inputs/exp.js
@@ -1,5 +1,6 @@
 import { Input } from '../input';
 import RestrictedInput from 'restricted-input';
+import { expirationDate } from 'card-validator'
 
 export default function ExpInput(options) {
   if (!options) options = {};
@@ -10,6 +11,39 @@ export default function ExpInput(options) {
   new RestrictedInput({
     element: el,
     pattern: '{{99}} / {{99}}'
+  });
+
+  el.addEventListener('input', function(e) {
+    if (el.value === '') {
+      options.channel.say('change', { 
+        type: 'exp',
+        empty: true
+      });
+
+      return;
+    }
+
+    const validationResult = expirationDate(el.value);
+
+    el.classList.remove("invalid");
+    el.classList.remove("complete");
+
+    if (validationResult.isPotentiallyValid !== true) {
+      options.channel.say('change', { 
+        type: 'exp',
+        isInvalid: true
+      });
+
+      el.classList.add("invalid");
+    } else {
+
+      if (validationResult.isValid === true) el.classList.add("complete");
+
+      options.channel.say('change', { 
+        type: 'exp',
+        complete: validationResult.isValid
+      });
+    }
   });
 
   return el;
